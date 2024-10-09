66e2927e58780bafb1846f0a8a675fad0c6eff1b
doudou <dushuying1996@gmail.com>
2024-10-09 10:02:13 -0700

    set up the main entry of the app: navigation between screens

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import ActivitiesScreen from './screens/ActivitiesScreen';
+import DietScreen from './screens/DietScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import AddActivityScreen from './screens/AddActivityScreen';
+import AddDietScreen from './screens/AddDietScreen';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
-export default function App() {
+function ActivitiesStack() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Diet" component={HomeScreen} />
-      <Tab.Screen name="Activities" component={SettingsScreen} />
-    </Tab.Navigator>
+    <Stack.Navigator>
+      <Stack.Screen name="Activities" component={ActivitiesScreen} />
+      <Stack.Screen name="AddActivity" component={AddActivityScreen} />
+    </Stack.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DietStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Diet" component={DietScreen} />
+      <Stack.Screen name="AddDiet" component={AddDietScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" backgroundColor="#ffffff" />  {/* Add StatusBar globally */}
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Activities" component={ActivitiesStack} />
+          <Tab.Screen name="Diet" component={DietStack} />
+          <Tab.Screen name="Settings" component={SettingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
+  );
+}
