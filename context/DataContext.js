8e5e30857d42a0e9e18fdce37186c28ea4f89b82
doudou <dushuying1996@gmail.com>
2024-10-09 11:39:05 -0700

    add option prop to each screen to display the title

---
+++ context/DataContext.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function DataContext() {
+    return (
+        <View>
+            <Text>DataContext</Text>
+        </View>
+    )
+}
---
+++ screens/ActivitiesScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ActivitiesScreen() {
+    return (
+        <View>
+            <Text>ActivitiesScreen</Text>
+        </View>
+    )
+}
