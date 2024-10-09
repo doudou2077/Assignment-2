71dc1b811efc854dc5ac98a458f7a8fae85a2a2d
doudou <dushuying1996@gmail.com>
2024-10-09 11:46:03 -0700

    pass the navigation and add an "add" button to the right top of the screen

--- screens/ActivitiesScreen.js
+++ screens/ActivitiesScreen.js
@@ -1,7 +1,18 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export default function ActivitiesScreen() {
+export default function ActivitiesScreen({ navigation }) {
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <Button
+                    onPress={() => navigation.navigate('AddActivity')}  // Navigate to AddActivityScreen
+                    title="Add"
+                />
+            ),
+        });
+    }, [navigation]);
+
     return (
         <View>
             <Text>ActivitiesScreen</Text>
