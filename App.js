24da804fba440970ebfd2d9ff421362e695e5a08
doudou <dushuying1996@gmail.com>
2024-10-09 09:36:58 -0700

    add the packages and corresponding screen and component js files

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Diet" component={HomeScreen} />
+      <Tab.Screen name="Activities" component={SettingsScreen} />
+    </Tab.Navigator>
   );
 }
 
