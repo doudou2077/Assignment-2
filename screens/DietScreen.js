a13f1004a0e5222de42e0b212fa148c340fa82e1
doudou <dushuying1996@gmail.com>
2024-10-09 22:40:15 -0700

    add an "add" button on the right top corner of the dietscreen to navigate to the adddietscreen

--- screens/DietScreen.js
+++ screens/DietScreen.js
@@ -1,10 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useLayoutEffect } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
-export default function DietScreen() {
+export default function DietScreen({ navigation }) {
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <Button
+                    onPress={() => navigation.navigate('AddDiet')}
+                    title="Add"
+                />
+            ),
+        });
+    }, [navigation]);
+
     return (
-        <View>
+        <View style={styles.container}>
             <Text>DietScreen</Text>
+            {/* add the list of diet entries here later */}
         </View>
-    )
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
