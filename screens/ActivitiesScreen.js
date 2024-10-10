4f0fb3e8e667a879825853d84bf5e5a366b3baff
doudou <dushuying1996@gmail.com>
2024-10-09 22:15:44 -0700

    add itemlist component to display the activities that passed from the context

--- screens/ActivitiesScreen.js
+++ screens/ActivitiesScreen.js
@@ -1,6 +1,7 @@
-import { View, Text, Button, FlatList, StyleSheet } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import React, { useLayoutEffect } from 'react';
+import { View, Button, StyleSheet } from 'react-native';
 import { useActivityContext } from '../context/ActivityContext';
+import ItemsList from '../components/ItemList';
 
 export default function ActivitiesScreen({ navigation }) {
     const { activities } = useActivityContext();
@@ -16,34 +17,16 @@ export default function ActivitiesScreen({ navigation }) {
         });
     }, [navigation]);
 
-    const renderItem = ({ item }) => (
-        <View style={styles.activityItem}>
-            <Text>Type: {item.type}</Text>
-            <Text>Duration: {item.duration} mins</Text>
-            <Text>Date: {item.date.toDateString()}</Text>
-        </View>
-    );
-
     return (
         <View style={styles.container}>
-            <FlatList
-                data={activities}
-                renderItem={renderItem}
-                keyExtractor={item => item.id.toString()}
-            />
+            <ItemsList items={activities} type="activity" />
         </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 10,
-    },
-    activityItem: {
-        padding: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-        marginBottom: 10,
     },
 });
