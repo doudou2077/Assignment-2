b11d53c222045e096f0fa8007e65804612296f34
doudou <dushuying1996@gmail.com>
2024-10-11 16:34:48 -0700

    update the style: apply the shared style for header and go back button

--- screens/AddActivityScreen.js
+++ screens/AddActivityScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Alert } from 'react-native'
+import { View, Text, TextInput, StyleSheet, TouchableOpacity, TouchableWithoutFeedback, Alert, SafeAreaView } from 'react-native'
 import React, { useState } from 'react'
 import DropDownPicker from 'react-native-dropdown-picker';
 import { useNavigation } from '@react-navigation/native';
@@ -48,59 +48,76 @@ export default function AddActivityScreen() {
     }
 
     return (
-        <TouchableWithoutFeedback onPress={closeDropDown}>
-            <View style={sharedStyles.container}>
-                <Text style={sharedStyles.label}>Activity *</Text>
-                <DropDownPicker
-                    open={open}
-                    value={activityType}
-                    items={
-                        [{ label: 'Walking', value: 'walking' },
-                        { label: 'Running', value: 'running' },
-                        { label: 'Swimming', value: 'swimming' },
-                        { label: 'Weights', value: 'weights' },
-                        { label: 'Yoga', value: 'yoga' },
-                        { label: 'Cycling', value: 'cycling' },
-                        { label: 'Hiking', value: 'hiking' },]
-                    }
-                    setOpen={setOpen}
-                    setValue={setActivityType}
-                    placeholder='Select An Activity'
-                    style={styles.dropdown}
-                    zIndex={3000}
-                    zIndexInverse={1000}
-                />
-
-                <Text style={sharedStyles.label}>Duration(mins) *</Text>
-                <TextInput
-                    style={styles.durationInput}
-                    placeholder=''
-                    keyboardType='numeric'
-                    value={duration}
-                    onChangeText={setDuration}
-                />
-
-                <DatePicker date={date} setDate={setDate} label="Date *" />
-
-                <View style={sharedStyles.buttonContainer}>
-                    <TouchableOpacity
-                        style={[sharedStyles.button, { backgroundColor: colors.secondary }]}
-                        onPress={handleCancel}
-                    >
-                        <Text style={sharedStyles.buttonText}>Cancel</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity
-                        style={[sharedStyles.button, { backgroundColor: colors.primary }]}
-                        onPress={handleSave}>
-                        <Text style={sharedStyles.buttonText}>Save</Text>
-                    </TouchableOpacity>
+        <View style={[sharedStyles.container]}>
+            <View style={sharedStyles.headerContainer}>
+                <View style={sharedStyles.headerTextContainer}>
+                    <Text style={sharedStyles.headerText}>Add Activity</Text>
                 </View>
-
+                <TouchableOpacity
+                    style={sharedStyles.goBackButton}
+                    onPress={() => handleCancel()}
+                >
+                    <Text style={sharedStyles.goBackButtonText}> &lt; </Text>
+                </TouchableOpacity>
             </View>
 
+            <TouchableWithoutFeedback onPress={closeDropDown}>
+                <View style={sharedStyles.centeredContainer}>
+                    <View style={sharedStyles.formElement}>
+                        <Text style={sharedStyles.label}>Activity *</Text>
+                        <DropDownPicker
+                            open={open}
+                            value={activityType}
+                            items={
+                                [{ label: 'Walking', value: 'walking' },
+                                { label: 'Running', value: 'running' },
+                                { label: 'Swimming', value: 'swimming' },
+                                { label: 'Weights', value: 'weights' },
+                                { label: 'Yoga', value: 'yoga' },
+                                { label: 'Cycling', value: 'cycling' },
+                                { label: 'Hiking', value: 'hiking' },]
+                            }
+                            setOpen={setOpen}
+                            setValue={setActivityType}
+                            placeholder='Select An Activity'
+                            style={styles.dropdown}
+                            zIndex={3000}
+                            zIndexInverse={1000}
+                        />
+                    </View>
 
-        </TouchableWithoutFeedback>
+                    <View style={sharedStyles.formElement}>
+                        <Text style={sharedStyles.label}>Duration(mins) *</Text>
+                        <TextInput
+                            style={styles.durationInput}
+                            placeholder=''
+                            keyboardType='numeric'
+                            value={duration}
+                            onChangeText={setDuration}
+                        />
+                    </View>
+
+                    <View style={sharedStyles.formElement}>
+                        <DatePicker date={date} setDate={setDate} label="Date *" />
+                    </View>
+                    <View style={sharedStyles.buttonContainer}>
+                        <TouchableOpacity
+                            style={[sharedStyles.button, { backgroundColor: colors.secondary }]}
+                            onPress={handleCancel}
+                        >
+                            <Text style={sharedStyles.buttonText}>Cancel</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity
+                            style={[sharedStyles.button, { backgroundColor: colors.primary }]}
+                            onPress={handleSave}>
+                            <Text style={sharedStyles.buttonText}>Save</Text>
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+            </TouchableWithoutFeedback>
+        </View>
     )
 }
 
