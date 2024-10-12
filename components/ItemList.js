9564744413c5b12fb53f2ae45854116e6fccef30
doudou <dushuying1996@gmail.com>
2024-10-11 20:52:28 -0700

    apply the updated style on the itemlist component

--- components/ItemList.js
+++ components/ItemList.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, StyleSheet } from 'react-native';
 import { listScreenStyles } from '../helperFile/listScreenStyles';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { colors } from '../helperFile/sharedStyles';
 
 const ItemsList = ({ items, type }) => {
+    const isSpecialActivity = (item) => {
+        return (item.type === 'running' || item.type === 'weights') && item.duration > 60;
+    };
     const renderItem = ({ item }) => (
-        <View style={listScreenStyles.listItem}>
+        <View style={[listScreenStyles.listItem, styles.listItemRow]}>
             {type === 'activity' ? (
                 <>
-                    <Text style={listScreenStyles.listItemTitle}>{item.type}</Text>
-                    <Text style={listScreenStyles.listItemSubtitle}>Duration: {item.duration} mins</Text>
-                    <Text style={listScreenStyles.listItemSubtitle}>Date: {item.date.toDateString()}</Text>
+                    <Text style={listScreenStyles.itemType}>{item.type}</Text>
+                    <View style={listScreenStyles.detailsContainer}>
+                        <View style={listScreenStyles.detailBox}>
+                            <Text style={listScreenStyles.detailText}>{item.date.toDateString()}</Text>
+                        </View>
+                        <View style={listScreenStyles.detailBox}>
+                            <Text style={listScreenStyles.detailText}>{item.duration} min</Text>
+                        </View>
+                    </View>
+                    {isSpecialActivity(item) && (
+                        <FontAwesome name="exclamation-triangle" size={24} color={colors.white} style={listScreenStyles.icon} />
+                    )}
                 </>
+
             ) : (
                 <>
                     <Text style={listScreenStyles.listItemTitle}>{item.description}</Text>
@@ -30,5 +45,16 @@ const ItemsList = ({ items, type }) => {
         />
     );
 };
+
+const styles = StyleSheet.create({
+    listItemRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    activityInfo: {
+        flex: 1,
+    },
+});
 
 export default ItemsList;
