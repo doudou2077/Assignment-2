aa7e7278c3f575df3fb0e4567a171facef9b808e
doudou <dushuying1996@gmail.com>
2024-10-12 12:14:09 -0700

    update one feature: If user wants to select the current date, they can dismiss the calendar by tapping the input

--- components/DatePicker.js
+++ components/DatePicker.js
@@ -1,21 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 const DatePicker = ({ date, setDate, label, showDatePicker, setShowDatePicker }) => {
 
     const onDateChange = (_, selectedDate) => {
+        // If user doesn't select a date, pick the current date
         const currentDate = selectedDate || date;
         setShowDatePicker(false);
         setDate(currentDate);
     };
 
     const handleDatePickerPress = () => {
-        if (!date) {
-            setDate(new Date());
+        if (showDatePicker) {
+            setShowDatePicker(false); // Close the date picker if it's already open
+        } else {
+            if (!date) {
+                setDate(new Date()); // Set current date if none is selected yet
+            }
+            setShowDatePicker(true); // Open the date picker
         }
-        setShowDatePicker(true);
     };
+
 
     return (
         <View>
