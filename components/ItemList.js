1b9e3f300cbc0a755c305ed5f23b10d480cae7e5
doudou <dushuying1996@gmail.com>
2024-10-22 20:02:34 -0700

    update the comment to avoid error: delete the one that inside the text component

--- components/ItemList.js
+++ components/ItemList.js
@@ -3,30 +3,34 @@ import { View, Text, FlatList } from 'react-native';
 import { listScreenStyles } from '../helperFile/listScreenStyles';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-// list component used in addactivity and add diet screens to avoid code duplication
+
 const ItemsList = ({ items, type }) => {
-    // Function to determine if an activity is special based on type and duration
+
+    // Function to determine if an activity is considered "special"
     const isSpecialActivity = (item) => {
         const lowercaseType = item.type.toLowerCase();
         const duration = Number(item.duration);
         return (lowercaseType === 'running' || lowercaseType === 'weights') && duration > 60;
     };
 
-    // Function to determine if a diet item is special based on calories
+
     const isSpecialDiet = (item) => {
         return item.calories > 800;
     };
 
+
+    // Render individual item in the list
     const renderItem = ({ item }) => {
         const special = type === 'activity' ? isSpecialActivity(item) : isSpecialDiet(item);
         return (
             <View style={listScreenStyles.listItem}>
-                // Display activities or description
+
                 <View style={listScreenStyles.typeContainer}>
                     <Text style={listScreenStyles.itemType}>
                         {type === 'activity' ? item.type : item.description}
                     </Text>
-                    // Icon for special items
+
+                    {/* Show warning icon for special items */}
                     {special && (
                         <FontAwesome
                             name="exclamation-triangle"
@@ -37,11 +41,11 @@ const ItemsList = ({ items, type }) => {
                     )}
                 </View>
                 <View style={listScreenStyles.detailsContainer}>
-                    // Display date
+
                     <View style={listScreenStyles.detailBox}>
                         <Text style={listScreenStyles.detailText}>{item.date.toDateString()}</Text>
                     </View>
-                    // Display duration or calories
+
                     <View style={listScreenStyles.detailBox}>
                         <Text style={listScreenStyles.detailText}>
                             {type === 'activity' ? `${item.duration} min` : `${item.calories} cal`}
